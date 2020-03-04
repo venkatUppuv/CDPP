5f8d8963855913ee3b06278fc8dd12a6625b4498
Deepika Gopala <c-dgopala@dovercorp.com>
2020-03-04 17:15:47 +0530

    removed &nbsp;

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import './SideNav.scss';
 
 export default class SideNav extends Component {
 
@@ -7,13 +8,13 @@ export default class SideNav extends Component {
         return (
             <div className='SideNav'>
                 <div className='Hamburg ' onClick={this.props.onToggle}><i class="fa fa-list" aria-hidden="true"></i></div>
-                <div>
+                <div class="linkItem">
                     <Link to='/home/projects'>
-                        <i class="fa fa-briefcase" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;{this.props.toggle && 'Projects'}
+                        <i class="fa fa-briefcase" aria-hidden="true"></i><span>{this.props.toggle && 'Projects'}</span>
                     </Link>
                 </div>
-                <div><i class="fa fa-user" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;&nbsp;{this.props.toggle && 'Vendors'}</div>
-                <div><i class="fa fa-users" aria-hidden="true"></i>&nbsp;&nbsp;{this.props.toggle && 'User Managment'}</div>
+                <div class="linkItem"><i class="fa fa-user" aria-hidden="true"></i><span>{this.props.toggle && 'Vendors'}</span></div>
+                <div class="linkItem"><i class="fa fa-users" aria-hidden="true"></i><span>{this.props.toggle && 'User Managment'}</span></div>
             </div>
         )
     }
