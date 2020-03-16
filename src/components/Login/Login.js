8e35775e7075c899fe8ae4cc81987260a57f00cc
Deepika Gopala <c-dgopala@dovercorp.com>
2020-03-16 19:18:42 +0530

    login api integration

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { InputGroup, InputGroupText, InputGroupAddon, Input } from 'reactstrap';
+import { Link, useHistory, withRouter } from 'react-router-dom'
+import { InputGroup, InputGroupText, InputGroupAddon, Input, Form, FormGroup, Label } from 'reactstrap';
 import logo from '../../assets/images/logo.jpg';
 import { Button } from 'reactstrap';
+import axios from 'axios';
 import './Login.scss';
 
-export default class Login extends Component {
+class Login extends Component {
+
+    componentDidMount(){
+       // axios.post();
+    }
+   
+
+    constructor(){
+        super();
+        this.state = {
+            username: '',
+            password:''
+        }
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    onSubmit = (e) => {
+        e.preventDefault();
+        if(this.state.username !== null && this.state.password !== null){
+            let url = 'http://localhost:8082/login?username='+this.state.username+'&password='+this.state.password;
+            axios.post(url).then((result) => {
+                this.props.history.push('/home');
+                axios.defaults.headers.common['Authorization'] = `Bearer ${result.accessToken}` 
+            });
+        }  
+     }
+ 
     render() {
         return (
             <div className='Login'>
@@ -17,26 +47,19 @@ export default class Login extends Component {
                         <p>engineering innovation</p>
                     </div>
 
-                    <div className='InputFields'>
-                        <InputGroup>
-                            <InputGroupAddon addonType="prepend">
-                                <InputGroupText>User Name</InputGroupText>
-                            </InputGroupAddon>
-                            <Input />
-                        </InputGroup>
-                        <br />
-                        <InputGroup>
-                            <InputGroupAddon addonType="prepend">
-                                <InputGroupText>Password&nbsp;&nbsp;&nbsp;</InputGroupText>
-                            </InputGroupAddon>
-                            <Input />
-                        </InputGroup>
-                        <br />
-                        <Link to='/home'>
-                            <Button color="primary">Login</Button>
-                        </Link>
-                    </div>
-
+                    <Form  className='InputFields'>
+                    <FormGroup>
+                      <Label for="username">Username</Label>
+                      <Input type="username" name="username" id="username" placeholder="username"   value={this.state.username}
+                      onChange={e => this.handleChange(e)}/>
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="password">Password</Label>
+                      <Input type="password" name="password" id="password" placeholder="password"   value={this.state.password}
+                      onChange={e => this.handleChange(e)}/>
+                    </FormGroup>
+                    <Button onClick={this.onSubmit}>Submit</Button>
+                    </Form>
                 </div>
 
                 <div className='LoginImage'>
@@ -47,3 +70,5 @@ export default class Login extends Component {
         )
     }
 }
+
+export default withRouter(Login)
